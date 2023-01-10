39e1274cb4f41040163b46f702d940e14c2cc843
vinmash <vinmash@mail.ru>
2023-01-10 22:33:53 +0500

    task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+    public static void task1() {
+        System.out.println("Задача 1");
+        int age = 17;
+        if (age >= 18) {
+            System.out.println("Человек совершеннолетний");
+        }
+        if (age < 18) {
+            System.out.println("Человек не достиг совершеннолетия, нужно немного подождать");
+        }
     }
 }
