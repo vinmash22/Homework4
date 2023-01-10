16cf0102beec74f5e646ee38028a34532afd0d44
vinmash <vinmash@mail.ru>
2023-01-10 22:47:03 +0500

    task 1-3

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,16 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
     public static void task1() {
         System.out.println("Задача 1");
         int age = 17;
         if (age >= 18) {
-            System.out.println("Если возраст человека равен " +age+ " , то он совершеннолетний");
+            System.out.println("Если возраст человека равен " +age+ ", то он совершеннолетний");
         }
         if (age < 18) {
-            System.out.println("Если возраст человека равен " +age+ " то он не достиг совершеннолетия, нужно немного подождать");
+            System.out.println("Если возраст человека равен " +age+ ", то он не достиг совершеннолетия, нужно немного подождать");
         }
     }
     public static void task2() {
@@ -21,6 +22,16 @@ public class Main {
         }
         if (temperature < 5) {
             System.out.println("На улице " +temperature+ " градусов, нужно надеть шапку");;
+        }
+    }
+    public static void task3() {
+        System.out.println("Задача 3");
+        int speed = 62;
+        if (speed > 60) {
+            System.out.println("Если скорость " +speed+ ", то придется заплатить штраф");
+        }
+        if (speed <= 60) {
+            System.out.println("Если скорость " +speed+ ", то можно ездить спокойно");
         }
     }
 }
