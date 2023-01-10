7e8188f4a6a0772eef840c72510276e356fac4fb
vinmash <vinmash@mail.ru>
2023-01-11 00:26:43 +0500

    task 1-7

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         task4();
         task5();
         task6();
+        task7();
     }
     public static void task1() {
         System.out.println("Задача 1");
@@ -40,7 +41,10 @@ public class Main {
     public static void task4() {
         System.out.println("Задача 4");
         int age = 18;
-        if (age >= 2 && age <= 6) {
+        if (age >= 24) {
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить на работу");
+        }
+        else {if (age >= 2 && age <= 6) {
             System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в детский сад");
         }
         if (age >= 7 && age < 18) {
@@ -49,8 +53,6 @@ public class Main {
         if (age >= 18 && age < 24) {
             System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в университет");
         }
-        if (age >= 24) {
-            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить на работу");
         }
     }
     public static void task5() {
@@ -84,6 +86,33 @@ public class Main {
         else  {
             System.out.println("Номер билета " + ticketNumber + ", стоячее место");
         }
+        }
+    }
+    public static void task7() {
+        System.out.println("Задача 7");
+        int one = 10;
+        int two = 15;
+        int three = 8;
+        boolean oneLessTwo = one < two;
+        if (oneLessTwo) {
+            System.out.println("Первое число " +one+ " меньше второго " +two);
+        }
+        else  {
+            System.out.println("Первое число " +one+ " больше второго " +two);
+        }
+        boolean twoLessThree = two < three;
+        if (twoLessThree) {
+            System.out.println("Второе число " +two+ " меньше третьего " +three);
+        }
+        else  {
+            System.out.println("Второе число " +two+ " больше третьего " +three);
+        }
+        boolean oneLessThree = one < three;
+        if (oneLessThree) {
+            System.out.println("Первое число " +one+ " меньше третьего " +three);
+        }
+        else  {
+            System.out.println("Первое число " +one+ " больше третьего " +three);
         }
     }
 }
